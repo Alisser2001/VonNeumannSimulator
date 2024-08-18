fcf0b1d1b86754941de4237b9206f9b6cfd34175
Alisser2001 <estiven.carmona@udea.edu.co>
2024-08-18 17:47:52 -0500

    Creacion de aplicacion y de servidor

---
+++ front-neumann/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex h-auto min-h-screen w-screen flex-col items-center justify-center">
+      
+    </main>
+  );
+}
