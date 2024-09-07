60956f7929e028e5c1e31e696da50a8d4d7d6da4
Alisser2001 <estiven.carmona@udea.edu.co>
2024-09-07 13:08:38 -0500

    Base de backend completamente funcional

--- back-neumann/memory.py
+++ back-neumann/memory.py
@@ -1,8 +1,8 @@
-def memory(tableOfMemory, r_dir, operation=None, value=None):
-    if operation is None: return tableOfMemory[r_dir]
-    if operation=="0110" and value is not None: 
-        tableOfMemory[r_dir] = value
+def memory(tableOfMemory, direction, operation=None, value=None):
+    if operation is None: return tableOfMemory[direction]
+    if operation=="0110": 
+        tableOfMemory[direction] = value
         return 
     if operation=="0000":
-        return tableOfMemory[r_dir]
+        return tableOfMemory[direction]
 
